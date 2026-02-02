6dfce334117f827a01f831f07f557ba956cf2138
madebysooga <cocojumpo53@gmail.com>
2026-02-02 20:38:33 +0100

    Update script.js

--- Mapa/script.js
+++ Mapa/script.js
@@ -1,1 +1,25 @@
+const MAP_SIZE = 8192;
+const bounds = [[0, 0], [MAP_SIZE, MAP_SIZE]];
 
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -4,
+    maxZoom: 2,
+    zoomControl: true,
+    dragging: true,
+    scrollWheelZoom: true,
+    touchZoom: true
+});
+
+L.imageOverlay(
+    "https://i.imgur.com/6Z8YB7m.jpg",
+    bounds
+).addTo(map);
+
+map.fitBounds(bounds);
+map.setView([MAP_SIZE / 2, MAP_SIZE / 2], -2);
+
+map.setMaxBounds(bounds);
+map.on("drag", () => {
+    map.panInsideBounds(bounds, { animate: false });
+});
